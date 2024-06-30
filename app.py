ff865dd7109272399edd208ac4f1ad8b363764e9
yash-raj202134 <yashraj3376@gmail.com>
2024-06-30 22:58:32 +0530

    static img resources added

--- app.py
+++ app.py
@@ -9,3 +9,5 @@ from recommendafy.utils import truncate
 app = Flask(__name__)
 
 # Loading files
+trending_products = pd.read_csv("data/trending_products.csv")
+train_data = pd.read_csv("data/clean_data.csv")
