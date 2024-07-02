02c2729d012bbd69cec26e5222033ae34a002e14
yash-raj202134 <yashraj3376@gmail.com>
2024-07-02 13:20:38 +0530

    app update

--- app.py
+++ app.py
@@ -34,6 +34,27 @@ def index():
     # Create a list of random image URLs for each product
     random_product_image_urls = [random.choice(random_image_urls) for _ in range(len(trending_products))]
     price = [40, 50, 60, 70, 100, 122, 106, 50, 30, 50]
-    return render_template('index.html',trending_products=trending_products.head(8),truncate = truncate,
-                           random_product_image_urls=random_product_image_urls,
+    return render_template('index.html',trending_products = trending_products.head(8),truncate = truncate,
+                           random_product_image_urls = random_product_image_urls,
                            random_price = random.choice(price))
+
+
+# routes
+@app.route("/main")
+def main():
+    return render_template('main.html')
+
+
+# routes
+@app.route("/index")
+def indexredirect():
+    # Create a list of random image URLs for each product
+    random_product_image_urls = [random.choice(random_image_urls) for _ in range(len(trending_products))]
+    price = [40, 50, 60, 70, 100, 122, 106, 50, 30, 50]
+    return render_template('index.html', trending_products = trending_products.head(8), truncate=truncate,
+                           random_product_image_urls = random_product_image_urls,
+                           random_price = random.choice(price))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
